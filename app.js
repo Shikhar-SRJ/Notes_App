27bdc194ec695ab9e3164c37b654b8a05db4861b
Shikhar-SRJ <ptksuraj@gmail.com>
2020-12-10 14:37:59 +0530

    basic add note and remove note

--- app.js
+++ app.js
@@ -1,10 +1,70 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const validator = require('validator')
 const notes = require('./notes.js')
+
+yargs.version('1.0.1')
+
+yargs.command({
+    command: 'add',
+    describe: 'Adding a new note!',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: "Remove a note",
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List out all the notes',
+    handler: function () {
+        console.log('Listing out the notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a specific note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
 
 // console.log(notes())
 // console.log(validator.isEmail('abc@xyz.in'))
 // console.log(validator.isURL('google.abc')) 
-console.log(chalk.green('Success!'))
-console.log(chalk.bold.red('Error...'))
-console.log(chalk.inverse.bold.green('Success'))
+// console.log(chalk.green('Success!'))
+// console.log(chalk.bold.red('Error...'))
+// console.log(chalk.inverse.bold.green('Success'))
+
+// console.log(process.argv)
+
+// console.log(yargs.argv)
+
+yargs.parse()
